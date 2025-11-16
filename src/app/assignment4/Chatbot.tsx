9d5db7d9c1f82396b897e51526a7d395bb8291fe
Bradley Charles <bradgcharles@gmail.com>
2025-11-16 15:17:31 -0800

    fix: Increase maxTokens input range in Chatbot to a maximum of 3000 for improved flexibility

--- src/app/assignment4/Chatbot.tsx
+++ src/app/assignment4/Chatbot.tsx
@@ -162,7 +162,7 @@ export default function Chatbot() {
             <input
               type="range"
               min="10"
-              max="300"
+              max="3000"
               step="10"
               value={maxTokens}
               onChange={(e) => setMaxTokens(parseInt(e.target.value))}
